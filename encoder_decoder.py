0d447d27d9afd668b68751ff08bf3762cef525dc
AndreiDob <dobrea.andrei1998@gmail.com>
2020-01-14 11:22:03 +0200

    Initial commit

---
+++ encoder_decoder.py
@@ -0,0 +1,29 @@
+
+def encode_str(data):
+    buf = bytearray()
+    for char in data:
+        num = ord(char)
+
+        # Lower case latin letters
+        if num >= 97 and num <= 122:
+            buf.append(num - 96)
+
+        # Space
+        elif num == 32:
+            buf.append(27)
+
+        else:
+            raise Exception("Too many bits")
+
+    return buf
+
+
+def decode_str(data):
+    result = ""
+    for num in data:
+        if num == 27:
+            result += " "
+        else:
+            result += chr(num + 96)
+    return result
+
